735eac3cc4c0a6b2c4283568f765bf02b0b657f1
JakubPargan <parganjakub@gmail.com>
2025-03-19 16:15:10 +0100

    Create SoundLoader.cpp

---
+++ assets/textures/assets/models/SoundLoader.cpp
@@ -0,0 +1,8 @@
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <iostream>
+
+void playSound(const char* filename) {
+    // Beispielhaftes Sound-Laden
+    std::cout << "Sound wird abgespielt: " << filename << std::endl;
+}
